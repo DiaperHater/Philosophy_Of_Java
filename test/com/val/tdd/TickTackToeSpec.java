47dc6aeafc4e083aba26b3d053b905e276144328
Princess <princess@vlr.com>
2018-03-09 01:58:08 +0300

    suka rabotaet its finaly works

--- test/com/val/tdd/TickTackToeSpec.java
+++ test/com/val/tdd/TickTackToeSpec.java
@@ -2,7 +2,7 @@ package com.val.tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Before;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
